fa1750c22f5b50bbc8b643965f5b9de6663f05fc
Joonatan Samuel <joonatan.samuel@gmail.com>
2015-03-24 23:42:33 +0200

    Spellid uues failis ja fix explosion range

--- game_classes.py
+++ game_classes.py
@@ -1,4 +1,5 @@
-import pygame, in_game, map_gen, main, random, math, itertools
+import pygame, random, math, itertools
+import in_game, main, map_gen, spells
 
 # Omadused, mida k6ik characterid peaksid omama
 class Character:
@@ -92,61 +93,7 @@ class player(Character):
 
     def shoot(self, mouse_click_pos):
         global bulletGroup
-        bullet = Bullet(self.pos, mouse_click_pos)
+        bullet = spells.Bullet(self.pos, mouse_click_pos)
         in_game.bulletGroup.add(bullet)
 
 
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, player_pos, mouse_click_pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([in_game.block_size/2,in_game.block_size/2])
-        self.image.fill((255, 255, 255))
-        self.rect = self.image.get_rect()
-        self.pos = player_pos
-        self.rect.x = self.pos[0] * in_game.block_size - in_game.camera_pos[0]
-        self.rect.y = self.pos[1] * in_game.block_size - in_game.camera_pos[1]
-        self.starttick = pygame.time.get_ticks()
-
-        self.damage = 10
-        self.lifetime = 30000
-        self.speed = 0.5
-
-        # how many blocks the bullet moves at a time
-        self.angle = math.atan2(mouse_click_pos[1] - self.pos[1], mouse_click_pos[0] - self.pos[0])
-        self.speed_x = self.speed*math.cos(self.angle)
-        self.speed_y = self.speed*math.sin(self.angle)
-
-
-    def update(self, screen):
-        self.pos[0] += self.speed_x
-        self.pos[1] += self.speed_y
-        self.rect.x = self.pos[0] * in_game.block_size + in_game.camera_pos[0]
-        self.rect.y = self.pos[1] * in_game.block_size + in_game.camera_pos[1]
-        self.collision_detect(screen)
-        self.delete()
-
-    def explode(self, radius):
-        try:
-            for dy, dx in itertools.product(range(-radius, radius+1), repeat=2):
-                    # kustutab kivi map_list'ist, uuendab kaarti ja minimapi
-                    in_game.map_list[round(self.pos[1] + self.speed_y + dy)][round(self.pos[0] + self.speed_x + dx)] = 0
-
-        except: pass
-
-    def collision_detect(self, screen):
-        global bulletGroup, map_list
-        try:
-            if in_game.map_list[round(self.pos[1] + self.speed_y)][round(self.pos[0] + self.speed_x)] != 0:
-                #in_game.map_list[round(self.pos[1] + self.speed_y)][round(self.pos[0] + self.speed_y)] = 0
-                self.explode(1)
-
-                in_game.draw_map_surface(in_game.block_size)
-                in_game.draw_minimap(in_game.mm_block_size, in_game.mm_surface_size)
-                in_game.draw_minimap(in_game.mm_block_size, in_game.mm_surface_size)
-
-                in_game.bulletGroup.remove(self)
-        except: in_game.bulletGroup.remove(self)
-
-    def delete(self):
-        if pygame.time.get_ticks() - self.starttick >= self.lifetime:
-            in_game.bulletGroup.remove(self)
---
+++ spells.py
@@ -0,0 +1,61 @@
+import in_game, map_gen, main, game_classes
+import math, itertools, pygame
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, pos, mouse_click_pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([in_game.block_size/2,in_game.block_size/2])
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.pos = pos
+        self.rect.x = self.pos[0] * in_game.block_size - in_game.camera_pos[0]
+        self.rect.y = self.pos[1] * in_game.block_size - in_game.camera_pos[1]
+        self.starttick = pygame.time.get_ticks()
+
+        self.damage = 10
+        self.lifetime = 30000
+        self.speed = 0.5
+
+        # how many blocks the bullet moves at a time
+        self.angle = math.atan2(mouse_click_pos[1] - self.pos[1], mouse_click_pos[0] - self.pos[0])
+        self.speed_x = self.speed*math.cos(self.angle)
+        self.speed_y = self.speed*math.sin(self.angle)
+
+
+    def update(self, screen):
+        self.pos[0] += self.speed_x
+        self.pos[1] += self.speed_y
+        self.rect.x = self.pos[0] * in_game.block_size + in_game.camera_pos[0]
+        self.rect.y = self.pos[1] * in_game.block_size + in_game.camera_pos[1]
+        self.collision_detect(screen)
+        self.delete()
+
+    def explode(self, radius):
+        radius = int((radius - 1) / 2)
+        try:
+            if radius != 0:
+                for dy, dx in itertools.product(range(-radius, radius+1), repeat=2):
+                        # kustutab kivi map_list'ist, uuendab kaarti ja minimapi
+                        in_game.map_list[round(self.pos[1] + dy)][round(self.pos[0] + dx)] = 0
+            else:
+                in_game.map_list[round(self.pos[1] + self.speed_y)][round(self.pos[0] + self.speed_x)] = 0
+
+        except: pass
+
+    def collision_detect(self, screen):
+        global bulletGroup, map_list
+        try:
+            if in_game.map_list[round(self.pos[1] + self.speed_y)][round(self.pos[0] + self.speed_x)] != 0:
+                #in_game.map_list[round(self.pos[1] + self.speed_y)][round(self.pos[0] + self.speed_y)] = 0
+                self.explode(3)
+
+                in_game.draw_map_surface(in_game.block_size)
+                in_game.draw_minimap(in_game.mm_block_size, in_game.mm_surface_size)
+                in_game.draw_minimap(in_game.mm_block_size, in_game.mm_surface_size)
+
+                in_game.bulletGroup.remove(self)
+        except: in_game.bulletGroup.remove(self)
+
+    def delete(self):
+        if pygame.time.get_ticks() - self.starttick >= self.lifetime:
+            in_game.bulletGroup.remove(self)
